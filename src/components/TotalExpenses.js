3ed3be0f46a80edd33e9c643065e28213307cd43
Anna Duong <aduong9417@gmail.com>
2023-12-30 17:15:36 -0800

    style TotalExpenses component

--- src/components/TotalExpenses.js
+++ src/components/TotalExpenses.js
@@ -1,11 +1,11 @@
 function TotalExpenses() {
   return(
-    <div>
-      <div>
+    <div id="total-content" className="flex-row space-between">
+      <div id="total-amount">
         <h2>Total this month</h2>
         <p>$478.33</p>
       </div>
-      <div>
+      <div id="total-increase" className="flex-col">
         <p>+2.4% from</p>
         <p>from last month</p>
       </div>
